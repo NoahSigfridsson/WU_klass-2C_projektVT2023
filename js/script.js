f6db2b65f1e8a33c3162cb7aabd3c356ca5d71d5
NoahSigfridsson <noah.sigfridsson@elev.ga.ntig.se>
2023-03-01 08:12:04 +0100

    28 februari

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-const arrow = document.getElementById('arroow_menu_switch')
+const arrow = document.getElementById('arrow_menu_switch')
 const linkList = document.getElementById('link_list')
 
-function toggleMenu(){
+function toggleMenu() {
 
-    linkList.classlist.toggle('link_list_visible')
+    linkList.classList.toggle('link_list_visible')
+    arrow.classList.toggle('arrow_rotated')
 
 }
 
