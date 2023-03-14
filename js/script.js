fdd9c4a930c156829048bb60b7f06252e82b8d47
NoahSigfridsson <noah.sigfridsson@elev.ga.ntig.se>
2023-03-14 15:37:43 +0100

    14 mars
    varukorg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const arrow = document.getElementById('arrow_menu_switch')
 const linkList = document.getElementById('link_list')
+const cartButtonsArray = document.getElementsByClassName('lagg_i_korg')
+const cartIndicator = document.getElementById('indicator_circle')
+var numOfItemsInCart = 0
+var i = 0
+
 
 function toggleMenu() {
 
@@ -7,7 +12,21 @@ function toggleMenu() {
     arrow.classList.toggle('arrow_rotated')
 
 }
-
 arrow.addEventListener('click', toggleMenu)
 
+
+function addItemToCart() {
+    numOfItemsInCart = numOfItemsInCart + 1;
+    document.getElementById('indicator_text').innerHTML = numOfItemsInCart
+}
+while(i < cartButtonsArray.length){
+    cartButtonsArray[i].addEventListener('click', addItemToCart, indicatorVisible)
+    i = i + 1
+}
+
+
+function indicatorVisible() {
+    cartIndicator.style.opacity(0.2)
+    console.log(cartIndicator)
+}
 
